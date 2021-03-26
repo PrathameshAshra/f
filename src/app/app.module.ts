cdc575a2f835e19d991ea5936ee59edb5623c532
Prathamesh Ashra <prathameshedu@gmail.com>
2021-03-26 21:23:19 +0530

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { RecommendationComponent } from './components/recommendation/recommendation.component';
+import { UspComponent } from './components/usp/usp.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LandingPageComponent,
+    RecommendationComponent,
+    UspComponent,
+    HeroComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
